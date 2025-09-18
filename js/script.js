09acff70d0084a2e639b539e024c859bf5fff120
Naoko Iwamoto <naomekko.i@gmail.com>
2025-09-18 14:16:09 +0900

    プロンプトで入力でひらがなで　”もみじまんじゅう”でログインできる それ以外は合言葉が間違っている consoleに表示

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const user_password = prompt("パスワードを入力してください");
+if (user_password === "もみじまんじゅう") {
+  console.log("ログイン完了");
+  alert("ログイン完了");
+} else {
+  console.log("パスワードが違います");
+  alert("パスワードが違います");
+}
